0317b9ef3b751e83033f163b2139a1c230cdffd2
Dmytro <dimakoua@gmail.com>
2023-09-27 04:15:21 +0300

    update navigation

--- src/navigation/TabNavigator.js
+++ src/navigation/TabNavigator.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import AddItemScreen from '../screens/AddItemScreen';
 import ShoppingListScreen from '../screens/ShoppingListScreen ';
 
 const Tab = createBottomTabNavigator();
@@ -8,8 +7,7 @@ const Tab = createBottomTabNavigator();
 function TabNavigator() {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Screen1" component={AddItemScreen}  options={{ headerShown: false }}/>
-      <Tab.Screen name="Screen2" component={ShoppingListScreen}  options={{ headerShown: false }}/>
+      <Tab.Screen name="shoppingList" component={ShoppingListScreen}  options={{ headerShown: false }}/>
     </Tab.Navigator>
   );
 }
