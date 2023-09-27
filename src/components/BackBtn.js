72be1ad87f4fb16cc5386ba7bfcdf8f594c714dd
Dmytro <dimakoua@gmail.com>
2023-09-27 04:15:09 +0300

    add buttons

---
+++ src/components/BackBtn.js
@@ -0,0 +1,32 @@
+import { useNavigation } from '@react-navigation/native';
+import React from 'react';
+import { View, StyleSheet, TouchableOpacity, Image } from 'react-native';
+
+const BackButton = () => {
+    const navigation = useNavigation();
+
+    // Handle the "Back" button press to navigate back to the previous screen
+    const handleBackPress = () => {
+        navigation.goBack();
+    };
+
+    return (
+        <View style={styles.backButton}>
+            <TouchableOpacity
+                onPress={handleBackPress}
+                style={styles.settingsButton}
+            >
+                <Image source={require('./../../assets/icons8-back-50.png')} style={styles.image} />
+            </TouchableOpacity>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    image: {
+        width: 35,
+        height: 35
+    }
+});
+
+export default BackButton;
