cc8eb5602cd33ab45b03b9f4845468497b1ec589
Dmytro <dimakoua@gmail.com>
2023-10-05 03:41:03 +0300

    add amination

--- src/components/ListRow.js
+++ src/components/ListRow.js
@@ -1,33 +1,63 @@
-import React, { useMemo } from 'react';
-import { View, TouchableWithoutFeedback, TextInput, Image, StyleSheet, Dimensions } from 'react-native';
+import React, { useRef } from 'react';
+import { View, TouchableWithoutFeedback, TextInput, Image, StyleSheet, Dimensions, Animated } from 'react-native';
+import { PanGestureHandler } from 'react-native-gesture-handler';
 
 const ListRow = ({ index, item, textInputsRefs, checkedItems, setItemText, handleEnterPress, setItemQuantity, removeItem, toggleItem }) => {
-    return useMemo(() => (
-        <View style={styles.checkboxWrap}>
-            <TouchableWithoutFeedback onPress={() => toggleItem(item)}>
-                <View style={[styles.checkbox, { backgroundColor: checkedItems.includes(item.id) ? 'green' : 'transparent' }]} />
-            </TouchableWithoutFeedback>
-            <TextInput
-                ref={(ref) => (textInputsRefs.current[index] = ref)} // Store the ref in the array
-                value={item.text}
-                onChangeText={(text) => setItemText(item, text)}
-                onSubmitEditing={() => handleEnterPress(index)}
-                style={[styles.input, { textDecorationLine: checkedItems.includes(item.id) ? 'line-through' : 'none' }]}
-            />
-            <TextInput
-                value={`${item.quantity}`}
-                onChangeText={(quantity) => setItemQuantity(item, quantity)}
-                keyboardType="numeric"
-                style={styles.quantityInput}
-            />
-            {item.mealItem ? null : (
-                <TouchableWithoutFeedback onPress={() => removeItem(item)}>
-                    <Image source={require('./../../assets/icons8-close-24.png')} style={styles.closeBtn} />
-                </TouchableWithoutFeedback>
-            )}
+    const translateX = useRef(new Animated.Value(0)).current;
 
-        </View>
-    ));
+    const RowItem = (
+        <Animated.View style={[styles.checkboxWrap, { transform: [{ translateX }] }]}>
+            <View style={styles.checkboxWrap}>
+                <TouchableWithoutFeedback onPress={() => toggleItem(item)}>
+                    <View style={[styles.checkbox, { backgroundColor: checkedItems.includes(item.id) ? 'green' : 'transparent' }]} />
+                </TouchableWithoutFeedback>
+                <TextInput
+                    ref={(ref) => (textInputsRefs.current[index] = ref)} // Store the ref in the array
+                    value={item.text}
+                    onChangeText={(text) => setItemText(item, text)}
+                    onSubmitEditing={() => handleEnterPress(index)}
+                    style={[styles.input, { textDecorationLine: checkedItems.includes(item.id) ? 'line-through' : 'none' }]}
+                />
+                <TextInput
+                    value={`${item.quantity}`}
+                    onChangeText={(quantity) => setItemQuantity(item, quantity)}
+                    keyboardType="numeric"
+                    style={styles.quantityInput}
+                />
+                {item.mealItem ? null : (
+                    <TouchableWithoutFeedback onPress={() => removeItem(item)}>
+                        <Image source={require('./../../assets/icons8-close-24.png')} style={styles.closeBtn} />
+                    </TouchableWithoutFeedback>
+                )}
+            </View>
+        </Animated.View>
+    );
+
+    const onSwipeableClose = (event) => {
+        const offsetX = event.nativeEvent.translationX;
+
+        if (offsetX > 20) {
+            toggleItem(item);
+
+            Animated.timing(translateX, {
+                toValue: 50,
+                duration: 500,
+                useNativeDriver: true,
+            }).start(() => {
+                Animated.timing(translateX, {
+                    toValue: 0,
+                    duration: 500,
+                    useNativeDriver: true,
+                }).start();
+            });
+        }
+    };
+
+    return (
+        <PanGestureHandler onHandlerStateChange={onSwipeableClose}>
+            {RowItem}
+        </PanGestureHandler>
+    );
 };
 
 const windowWidth = Dimensions.get('window').width;
