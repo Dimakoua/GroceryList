255c958c53b942a094fc36a8612c5946bec6db9e
Dmytro <dimakoua@gmail.com>
2023-10-07 04:57:19 +0300

    add translations

--- src/translations/i18n.config.js
+++ src/translations/i18n.config.js
@@ -1,22 +1,53 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import { ua, en } from "../translations";
+import { ua, en, es, fr, de, pt, zh, ja, ko, it } from "../translations";
+import { findBestLanguageTag, getLocales } from "react-native-localize";
 
+console.log(getLocales());
 //empty for now
 const resources = {
-    en: {
+    'en': {
         translation: en,
     },
-    ua: {
+    'uk': {
         translation: ua,
+    },
+    'es': {
+        translation: es,
+    },
+    'fr': {
+        translation: fr,
+    },
+    'de': {
+        translation: de,
+    },
+    'pt': {
+        translation: pt,
+    },
+    'zh': {
+        translation: zh,
+    },
+    'ja': {
+        translation: ja,
+    },
+    'ko': {
+        translation: ko,
+    },
+    'it': {
+        translation: it,
     }
 };
+console.log(Object.keys(resources))
+const primaryLanguage = findBestLanguageTag(Object.keys(resources));
+console.log("primaryLanguage", primaryLanguage)
+
 
 i18n.use(initReactI18next).init({
     compatibilityJSON: 'v3',
     resources,
+    lng: primaryLanguage.languageTag, // Default language
     //language to use if translations in user language are not available
-    fallbackLng: "ua",
+    fallbackLng: "en",
     interpolation: {
         escapeValue: false, // not needed for react!!
     },
--- src/translations/index.js
+++ src/translations/index.js
@@ -1,2 +1,10 @@
-export { default as en } from "./en.json";
-export { default as ua } from "./ua.json";
+export { default as ua } from './ua.json';
+export { default as en } from './en.json';
+export { default as es } from './es.json';
+export { default as fr } from './fr.json';
+export { default as de } from './de.json';
+export { default as pt } from './pt.json';
+export { default as zh } from './zh.json';
+export { default as ja } from './ja.json';
+export { default as ko } from './ko.json';
+export { default as it } from './it.json';
